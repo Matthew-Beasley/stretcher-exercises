8bb30f57766cbb2bd23fb62972a2cb9870fd28b9
conbec <56614504+conbec@users.noreply.github.com>
2020-03-26 14:56:25 -0700

    solved dryStateHolder

---
+++ dryStateHolder.js
@@ -0,0 +1,46 @@
+class StateHolder {
+  constructor(start) {
+    this.state = start;
+    this.listeners = [];
+    this.stateRecord = [];
+  }
+  getState() {
+    return this.state;
+  }
+  sub(listener) {
+    this.listeners.push(listener);
+  }
+  increase(value) {
+    this.state += value;
+    this.stateRecord.push(this.state);
+  }
+  decrease(value) {
+    this.state -= value;
+    this.stateRecord.push(this.state);
+  }
+
+  broadcast() {
+    this.stateRecord.forEach(message => this.listeners.forEach(listener => listener(message)));
+  }
+}
+const stateHolder = new StateHolder(2);
+
+stateHolder.sub((state) => {
+  console.log(state);
+})
+
+stateHolder.increase(7);
+stateHolder.decrease(3);
+stateHolder.decrease(6);
+stateHolder.increase(2);
+
+stateHolder.broadcast();
+
+console.log(stateHolder.getState());
+/*
+9
+6
+0
+2
+2
+*/
