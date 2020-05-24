a791d493fcfbc02f3001adb2969880fd3dcf9112
conbec <56614504+conbec@users.noreply.github.com>
2020-05-24 14:34:29 -0700

    made algo linear

--- anagram.js
+++ anagram.js
@@ -1,10 +1,11 @@
+/* eslint-disable no-loop-func */
+
 const detectAnagram = (input) => {
   let tempWordA;
   let tempWordB;
   let outputCount = 1;
   const containsArr = [];
   let output = '';
-  let set = new Set();
 
   for (let i = 0; i < input.length; i++) {
     tempWordA = null
@@ -37,6 +38,45 @@ const detectAnagram = (input) => {
 3. tape, pate
 */
 
-const words = ['cat', 'act', 'ignore', 'a phrase', 'tape', 'pate', 'e hpsara'];
+//const words = ['cat', 'act', 'ignore', 'a phrase', 'tape', 'pate', 'e hpsara'];
 
-console.log(detectAnagram(words));
+//console.log(detectAnagram(words));
+
+
+const detectAnagramAgain = (input) => {
+  const storage = {};
+  let retStr = '';
+  input.forEach(origStr => {
+    const sortedStr = [...origStr].sort().join('').toLowerCase();
+    if (storage.hasOwnProperty(sortedStr)) {
+      storage[sortedStr].push(origStr);
+
+    } else {
+      storage[sortedStr] = [];
+      storage[sortedStr].push(origStr);
+    }
+  });
+
+  let count = 1;
+  for (let key in storage) {
+    if (storage[key] > 1) {
+      retStr += `${count}. `;
+      storage[key].forEach(word => {
+        retStr += `${word} `
+      });
+      retStr += '\n';
+      count++;
+    }
+  }
+  return retStr;
+}
+
+/*
+1. cat, act
+2. a phrase, e hpsara
+3. tape, pate
+*/
+
+const morewords = ['cat', 'act', 'ignore', 'a phrase', 'tape', 'pate', 'e hpsara'];
+
+console.log(detectAnagramAgain(morewords));
